2c421d3b8dc3f236c537ec309069d26453e46ff9
JaredDyreson <jared.dyreson@gmail.com>
2021-02-09 16:15:08 -0800

    added some more notes about  Python program structure

--- algorithms/dfs/main.py
+++ algorithms/dfs/main.py
@@ -22,28 +22,39 @@ graph = {
 
 def l_union(a: list, b: list):
     # source : https://stackoverflow.com/questions/9792664/converting-a-list-to-a-set-changes-element-order
-    # find the union of two sets <- probably should use in Funnel
+    # find the union of two lists <- probably should use in Funnel
 
     if(not isinstance(a, list) or
        not isinstance(b, list)):
        raise ValueError('two lists please')
     return [x for x in a if x not in b]
 
-X = None
-open_nodes = ["A"]
-goal_nodes = ["X"]
-closed = []
+# parameters have their desired types
+def dfs(G: dict, S: list, GD: list) -> bool: # -> (TYPE BEING RETURNED)
+    # type enforcement
+    if(not isinstance(S, list) or
+       not isinstance(GD, list) or
+       not isinstance(G, dict)):
+       raise ValueError
 
-while(open_nodes):
-    X = open_nodes.pop()
-    print(f'Current node {X}')
-    if(X in goal_nodes):
-        print("FOUND IT")
-        break
-    children = l_union(graph[X], open_nodes)
-    open_nodes[:0] = children
-    print(open_nodes)
-    closed.append(X)
+    X = None
+    open_nodes = S # renaming
+    goal_nodes = GD # renaming
+    closed = []
 
-if(not X):
-    print('failed')
+    while(open_nodes): # while len(open_nodes) > 0, python is cool
+        X = open_nodes.pop()
+        if(X in goal_nodes):
+            return True
+        children = l_union(graph[X], open_nodes)
+        open_nodes[:0] = children # prepend the contents of the lookup while removing duplicates from both containers
+        closed.append(X) # current node exhausted
+
+    return False
+
+resultant = dfs(graph, ["A"], ["X"])
+
+if(resultant):
+    print("[INFO] We found a result")
+else:
+    print("[ERROR] We could not find a result")
